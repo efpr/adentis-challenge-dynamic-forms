5687be31af63054b11fa73df05b3d978a5dd5a69
Eduardo Romão <eduardo.romao@ext.leroymerlin.pt>
2024-10-16 03:24:24 +0100

    🧪 Add test to validate the body

--- app/AdentisChallenge.DynamicForms.API.Test/CreateCompany.cs
+++ app/AdentisChallenge.DynamicForms.API.Test/CreateCompany.cs
@@ -1,7 +1,5 @@
 using DynamicForms.Core.CompanyAggregator;
-using DynamicForms.Core.Domain;
 using DynamicForms.Presentation.Endpoints;
-using MongoDB.Driver;
 using Moq;
 using System.Net;
 using System.Net.Http.Json;
@@ -18,7 +16,20 @@ namespace AdentisChallenge.DynamicForms.API.Test
 
         private readonly CreateCompanyRequest createCompanyRequest = new CreateCompanyRequest
         {
-            Name = "Company ABC"
+            Name = "Company ABC",
+            FormFields = new List<CreateCompanyFieldsRequest>
+            {
+                new CreateCompanyFieldsRequest
+                {
+                    Label = "Field 1",
+                    Type = "text",
+                    Validation = new CreateCompanyValidationRequest
+                    {
+                        Required = true,
+                        Pattern = "^[a-zA-Z0-9]*$"
+                    }
+                }
+            }
         };
 
         private readonly Company company;
@@ -29,7 +40,9 @@ namespace AdentisChallenge.DynamicForms.API.Test
             
             SetupMockRepository();
 
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
             var api = new CompanyApiFactory(_mockRepository.Object);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
             httpClient = api.CreateClient();
 
         }
@@ -59,17 +72,76 @@ namespace AdentisChallenge.DynamicForms.API.Test
         {
             var response = await httpClient.PostAsJsonAsync(_route, company);
 
-            
             Assert.Equal(HttpStatusCode.Created, response.StatusCode);
             _mockRepository.Verify(x => x.AddAsync(It.Is<Company>(c => c.Name == "Company ABC")), Times.Once);
         }
 
         [Fact]
-        public async Task Should_return_400_bad_request_When_send_invalid_company()
+        public async Task Should_return_400_bad_request_When_send_invalid_company_name()
         {
             var company = new CreateCompanyRequest()
             {
                 Name = string.Empty
+            };
+
+            var response = await httpClient.PostAsJsonAsync(_route, company);
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Should_return_400_bad_request_When_send_invalid_company_form_fields_label_missing()
+        {
+            var company = new CreateCompanyRequest()
+            {
+                Name = "Company ABC",
+                FormFields = new List<CreateCompanyFieldsRequest>
+                {
+                    new CreateCompanyFieldsRequest
+                    {
+                        Type = "text",
+                    },
+                }
+            };
+
+            var response = await httpClient.PostAsJsonAsync(_route, company);
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Should_return_400_bad_request_When_send_invalid_company_form_fields_type_missing()
+        {
+            var company = new CreateCompanyRequest()
+            {
+                Name = "Company ABC",
+                FormFields = new List<CreateCompanyFieldsRequest>
+                {
+                    new CreateCompanyFieldsRequest
+                    {
+                        Label = "Field 1",
+                    },
+                }
+            };
+
+            var response = await httpClient.PostAsJsonAsync(_route, company);
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Should_return_400_bad_request_When_send_invalid_company_form_fields_type_invalid()
+        {
+            var company = new CreateCompanyRequest()
+            {
+                Name = "Company ABC",
+                FormFields = new List<CreateCompanyFieldsRequest>
+                {
+                    new CreateCompanyFieldsRequest
+                    {
+                        Label = "Field 1",
+                    },
+                }
             };
 
             var response = await httpClient.PostAsJsonAsync(_route, company);
